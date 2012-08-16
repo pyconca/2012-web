95a05698b0cd7918c5dce88ba1a36840781281dc
Taavi Burns <taavi@taaviburns.ca>
2012-08-16 17:01:45 -0400

    Provide something approximating functional tests.
    Mostly, this just demonstrates how you can hit a controller when logged in or not logged in, with particular database fixtures. It's a worthy start.

--- pyconca/tests.py
+++ pyconca/tests.py
@@ -2,36 +2,91 @@ import unittest
 import transaction
 
 from pyramid import testing
+from sqlalchemy import create_engine
 
 from .models import DBSession
 
-class TestIndex(unittest.TestCase):
+# This duck will QUACK
+import pyconca
+pyconca.includeme = lambda configurator: None
+
+
+class TestIndexFunctional(unittest.TestCase):
     def setUp(self):
         self.request = testing.DummyRequest()
+        self.request.user = None
         self.config = testing.setUp(request=self.request)
+        self.config.include('pyconca')
+
+        engine = create_engine('sqlite://')
+        DBSession.configure(bind=engine)
+
+        from .models import (
+            Base,
+            User,
+            UserGroup,
+            Group,
+            )
+        Base.metadata.create_all(engine)
+        with transaction.manager:
+            self._admin_id = 1
+            self._speaker_id = 2
+            for model in (
+                User(id=self._admin_id, username='admin', password='', first_name='Admin', last_name='Istrator', email='example@example.com'),
+                User(id=self._speaker_id, username='speaker', password='', first_name='Spe', last_name='Aker', email='speaker@example.com'),
+                Group(id=1, name='admin'),
+                UserGroup(user_id=self._admin_id, group_id=1),
+                ):
+                DBSession.add(model)
 
     def tearDown(self):
         DBSession.remove()
         testing.tearDown()
 
+    def _loggedInAdmin(self):
+        from .models import User
+        self.request.user = DBSession.query(User).get(self._admin_id)
+        self.config.testing_securitypolicy(
+            userid='pyramid_admin',
+            permissive=True)
+
+    def _loggedInStaff(self):
+        from .models import User
+        self.request.user = DBSession.query(User).get(self._speaker_id)
+        self.config.testing_securitypolicy(
+            userid='pyramid_speaker',
+            permissive=True)
+
     def test_index_not_logged_in(self):
         from .views import index
-        self.request.user = None
 
         info = index(self.request)
 
         self.assertEquals(None, info['logged_in'])
         self.assertFalse(info['is_admin'])
 
-    def test_index_logged_in(self):
+    def test_index_logged_in_admin(self):
         from .views import index
-        self.config.testing_securitypolicy(
-            userid='hank',
-            permissive=True)
-        self.config.add_route('login', '/login')
-        self.request.user = None
 
-        info = index(self.request)
+        with transaction.manager:
+            self._loggedInAdmin()
+            info = index(self.request)
 
-        self.assertEquals('hank', info['logged_in'])
+        # testing_securitypolicy gives us pyramid_admin
+        self.assertEquals('pyramid_admin', info['logged_in'])
+
+        # Having the fixtures and remember()ing user_id=1 gives us is_admin
+        self.assertTrue(info['is_admin'])
+
+    def test_index_logged_in_speaker(self):
+        from .views import index
+
+        with transaction.manager:
+            self._loggedInStaff()
+            info = index(self.request)
+
+        # testing_securitypolicy gives us pyramid_speaker
+        self.assertEquals('pyramid_speaker', info['logged_in'])
+
+        # Having the fixtures and remember()ing user_id=1 gives us is_admin
         self.assertFalse(info['is_admin'])
