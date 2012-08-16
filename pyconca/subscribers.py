bfccd69901643ca056c6fb383e35846ea2a84907
Mathieu Leduc-Hamel <m@highscorehouse.com>
2012-08-16 00:35:17 -0400

    Mark all strings as translatable

---
+++ pyconca/subscribers.py
@@ -0,0 +1,16 @@
+from pyramid.i18n import get_localizer, TranslationStringFactory
+
+def add_renderer_globals(event):
+    request = event.request
+    event['_'] = request.translate
+    event['localizer'] = request.localizer
+
+tsf = TranslationStringFactory('pyconca')
+
+def add_localizer(event):
+    request = event.request
+    localizer = get_localizer(request)
+    def auto_translate(string):
+        return localizer.translate(tsf(string))
+    request.localizer = localizer
+    request.translate = auto_translate
