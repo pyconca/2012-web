795e11800d827e793f6f5b8a2f1b2846357831d6
Taavi Burns <taavi@taaviburns.ca>
2012-08-16 00:04:21 -0400

    Make test pass. Add another test that does something ever-so-slightly-more than a syntax check.

--- pyconca/tests.py
+++ pyconca/tests.py
@@ -7,26 +7,31 @@ from .models import DBSession
 
 class TestIndex(unittest.TestCase):
     def setUp(self):
-        self.config = testing.setUp()
-        from sqlalchemy import create_engine
-        engine = create_engine('sqlite://')
-        from .models import (
-            Base,
-            MyModel,
-            )
-        DBSession.configure(bind=engine)
-        Base.metadata.create_all(engine)
-        with transaction.manager:
-            model = MyModel(name='one', value=55)
-            DBSession.add(model)
+        self.request = testing.DummyRequest()
+        self.config = testing.setUp(request=self.request)
 
     def tearDown(self):
         DBSession.remove()
         testing.tearDown()
 
-    def test_it(self):
+    def test_index_not_logged_in(self):
         from .views import index
-        request = testing.DummyRequest()
-        info = index(request)
-        self.assertEqual(info['one'].name, 'one')
-        self.assertEqual(info['project'], 'pyconca')
+        self.request.user = None
+
+        info = index(self.request)
+
+        self.assertEquals(None, info['logged_in'])
+        self.assertFalse(info['is_admin'])
+
+    def test_index_logged_in(self):
+        from .views import index
+        self.config.testing_securitypolicy(
+            userid='hank',
+            permissive=True)
+        self.config.add_route('login', '/login')
+        self.request.user = None
+
+        info = index(self.request)
+
+        self.assertEquals('hank', info['logged_in'])
+        self.assertFalse(info['is_admin'])
