888b305e45c83b33f6ef7e64a7c18290a4c2168c
Taavi Burns <taavi@taaviburns.ca>
2012-09-17 21:33:50 -0400

    Make flash messages escaped-by-default in the template. Message producers must send something with an __html__() method to get actual markup dumped into the page.

--- pyconca/api/user_api.py
+++ pyconca/api/user_api.py
@@ -8,6 +8,7 @@ from pyramid.security import remember
 
 from pyconca.api.base_api import BaseApi
 from pyconca.dao.user_dao import UserDao
+from pyconca.markup import ContainsIntentionalMarkup
 from pyconca.security import generate_password
 
 
@@ -27,8 +28,9 @@ class UserApi(BaseApi):
 
     def _create_flash(self, user):
         msg = ('You have signed up for PyCon Canada!')
-        alert = ('To register for PyCon Canada 2012 in Toronto, go to \
-                 <a href="http://guestlistapp.com/events/116013">here</a>.')
+        alert = ContainsIntentionalMarkup(
+            'To register for PyCon Canada 2012 in Toronto, go to '
+            '<a href="http://guestlistapp.com/events/116013">here</a>.')
         self.request.session.flash(msg, 'success')
         self.request.session.flash(alert, 'alert')
 
