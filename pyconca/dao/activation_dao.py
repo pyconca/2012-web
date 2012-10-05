d6b06fe550aaeb730f1536eabd5c6c1fcdad812c
Kay Zhu <me@kayzhu.com>
2012-10-05 00:08:08 -0400

    Check if the reactivation link has expired, change expiration time from 7 to 3 days.

--- pyconca/dao/activation_dao.py
+++ pyconca/dao/activation_dao.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from pyconca.dao.base_dao import BaseDao
 from pyconca.models import Activation
 from pyconca.models import User
@@ -8,6 +10,15 @@ class ActivationDao(BaseDao):
     def __init__(self, authenticated_user):
         BaseDao.__init__(self, authenticated_user, Activation)
 
+    def check_valid(self, activation_code):
+        res = self._query().filter_by(code=activation_code).first()
+        if res:
+            if res.valid_until < datetime.now():
+                return False
+            return True
+        else:
+            return False
+
     def get_user_by_code(self, username, code):
         """Get the user for this code"""
         res = self._query().filter_by(code=code) \
@@ -17,7 +28,6 @@ class ActivationDao(BaseDao):
             return res.user
         else:
             return None
-        pass
 
     def set_new_pwd(self, username, code, new_pwd):
         res = self._query().filter_by(code=code) \
